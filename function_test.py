9e9d2c7a50a8258ad445ba72a8155824cfd2c929
Uzair <Mohammad_Uzair.Akram@tufts.edu>
2020-01-22 11:29:59 -0500

    Homework 1,2 and 3

---
+++ function_test.py
@@ -0,0 +1,9 @@
+# This part of the code defines a function
+def multiply(a,b):
+    return a * b
+
+# This part of the code then calls that function and assigns the variable x to its return value
+x = multiply(8,9)
+
+# This prints x to the terminal for humans to read
+print(x)
